d2f3dd894777dc5159424121840efecf0be3b7ba
Bhupender Rawat <bhupenderrawat742@gmail.com>
2021-07-27 09:41:22 +0530

    Initial Commit

---
+++ ListingUI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ListingUI
+//
+//  Created by Bhupender Rawat on 27/07/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
